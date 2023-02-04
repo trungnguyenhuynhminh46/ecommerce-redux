95280013892d1122a973bb701bd9632a17112151
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2023-02-04 10:56:54 +0700

    fix common

--- src/components/Common.tsx
+++ src/components/Common.tsx
@@ -8,15 +8,15 @@ interface PropsCommon {
 
 const Common: React.FC<PropsCommon> = ({ title }) => {
   return (
-    <section
-      className="relative w-full h-[200px] flex justify-center items-center"
-      style={{
-        background: "url(src/assets/images/common.avif)",
-        backgroundSize: "cover",
-      }}
-    >
+    <section className="relative w-full h-[200px] flex justify-center items-center">
+      {/* Background */}
+      <img
+        src="src/assets/images/common.avif"
+        alt=""
+        className="h-full w-full object-cover z-0"
+      />
       {/* overlay */}
-      <div className="absolute inset-0 bg-gray-900 opacity-60 flex justify-center items-center"></div>
+      <div className="absolute inset-0 bg-gray-900 opacity-60 flex justify-center items-center z-10"></div>
       {/* Content */}
       <div className="absolute inset-0 flex justify-center items-center z-20">
         <span className="text-2xl text-white font-bold">{title}</span>
